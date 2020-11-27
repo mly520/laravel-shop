bd850ee96c65f3a9048edbf310c1c54486867bb3
MAX <mly520@gmail.com>
2020-11-27 11:23:38 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+        </p>
+
+        <p class="float-right"><a href="mailto:76530299@qq.com">联系我们</a></p>
+    </div>
+</footer>
