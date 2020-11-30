8531e8a43ecd17ff1c325e21041529af8c9b8e13
MAX <mly520@gmail.com>
2020-11-30 15:24:07 +0800

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
