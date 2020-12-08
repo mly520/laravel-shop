b642c98dea07676a2a9a70c2a749130f25bd27a5
MA <76530299@qq.com>
2020-12-08 08:45:59 +0000

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,14 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+    // 订单支付
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+
 });
+// 订单支付
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 // 产品相关
 Route::get('products', 'ProductsController@index')->name('products.index');
