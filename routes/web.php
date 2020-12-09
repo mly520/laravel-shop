adfe40e8d95c2a5743d2025d8a943d5e5ce3e3b5
MA <76530299@qq.com>
2020-12-09 06:02:26 +0000

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
 
+    // 申请退款
+    Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
 });
 // 订单支付
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
